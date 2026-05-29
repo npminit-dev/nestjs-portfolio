2768e076a047502af8c79258c82212769f99a811
Jorge Antonio Balsamo <jorgeb.dev.acc@gmail.com>
2026-05-29 13:27:27 -0300

    feat: enable web analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['primeicons/primeicons.css', '~/assets/css/variables.css'],
-  modules: ['@nuxtjs/i18n'],
+  modules: ['@nuxtjs/i18n', '@vercel/analytics'],
   i18n: {
     locales: [
       { code: 'en', iso: 'en-US', name: 'English', file: 'en.json' },
